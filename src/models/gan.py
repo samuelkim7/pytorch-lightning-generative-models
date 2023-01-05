b1de946516f57562926f6c3d29f4a5304b269e04
samuelkim7 <samuelmwkim@gmail.com>
2023-01-06 01:19:03 +0900

    Added mnist_datamodule, gan, train_gan

---
+++ src/models/gan.py
@@ -0,0 +1,154 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import pytorch_lightning as pl
+
+
+class Generator(nn.Module):
+    def __init__(
+        self, 
+        latent_dim, 
+        img_shape,
+    ):
+        super().__init__()
+        self.img_shape = img_shape
+
+        def block(
+            in_feature, 
+            out_feature, 
+            normalize=True
+        ):
+            layers = [nn.Linear(in_feature, out_feature)]
+            if normalize:
+                layers.append(nn.BatchNorm1d(out_feature, 0.8))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+        
+        self.model = nn.Sequential(
+            *block(latent_dim, 128, normalize=False),
+            *block(128, 256),
+            *block(256, 512),
+            *block(512, 1024),
+            nn.Linear(1024, int(np.prod(img_shape))),
+            nn.Tanh(),
+        )
+    
+    def forward(self, z):
+        img = self.model(z)
+        img = img.view(img.size(0), *self.img_shape)
+        return img
+
+
+class Discriminator(nn.Module):
+    def __init__(self, img_shape):
+        super().__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(int(np.prod(img_shape)), 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, img):
+        img_flat = img.view(img.size(0), -1)
+        validity = self.model(img_flat)
+
+        return validity
+
+
+class GAN(pl.LightningModule):
+    def __init__(
+        self,
+        channels,
+        width,
+        height,
+        latent_dim: int = 100,
+        lr: float = 0.0002,
+        b1: float = 0.5,
+        b2: float = 0.999,
+        batch_size: int = 256,
+        **kwargs,
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+
+        data_shape = (channels, width, height)
+        self.generator = Generator(
+            latent_dim=self.hparams.latent_dim,
+            img_shape=data_shape,
+        )
+        self.discriminator = Discriminator(
+            img_shape=data_shape,
+        )
+
+        self.validation_z = torch.randn(8, self.hparams.latent_dim)
+
+        self.example_input_arrray = torch.zeros(2, self.hparams.latent_dim)
+    
+    def forward(self, z):
+        return self.generator(z)
+    
+    def adversarial_loss(self, y_hat, y):
+        return F.binary_cross_entropy(y_hat, y)
+    
+    def training_step(self, batch, batch_idx, optimizer_idx):
+        imgs, _ = batch
+
+        z = torch.randn(imgs.shape[0], self.hparams.latent_dim)
+        z = z.type_as(imgs)
+
+        # train generator
+        if optimizer_idx == 0:
+            valid = torch.ones(imgs.size(0), 1)
+            valid = valid.type_as(imgs)
+
+            g_loss = self.adversarial_loss(
+                self.discriminator(self(z)), 
+                valid,
+            )
+            self.log("g_loss", g_loss, prog_bar=True)
+            return g_loss
+        
+        # train discriminator
+        if optimizer_idx == 1:
+            valid = torch.ones(imgs.size(0), 1)
+            valid = valid.type_as(imgs)
+
+            real_loss = self.adversarial_loss(
+                self.discriminator(imgs),
+                valid,
+            )
+
+            fake = torch.zeros(imgs.size(0), 1)
+            fake = fake.type_as(imgs)
+
+            fake_loss = self.adversarial_loss(
+                self.discriminator(self(z).detach()), 
+                fake,
+            )
+
+            d_loss = (real_loss + fake_loss) / 2
+            self.log("d_loss", d_loss, prog_bar=True)
+            return d_loss
+
+    def configure_optimizers(self):
+        lr = self.hparams.lr
+        b1 = self.hparams.b1
+        b2 = self.hparams.b2
+
+        opt_g = torch.optim.Adam(self.generator.parameters(), lr=lr, betas=(b1, b2))
+        opt_d = torch.optim.Adam(self.discriminator.parameters(), lr=lr, betas=(b1, b2))
+        return [opt_g, opt_d], []
+
+    def on_train_epoch_end(self):
+        z = self.validation_z.type_as(self.generator.model[0].weight)
+
+        # log sampled images
+        sample_imgs = self(z)
+        grid = torchvision.utils.make_grid(sample_imgs)
+        self.logger.experiment.add_image("generated_images", grid, self.current_epoch)
---
+++ src/train_gan.py
@@ -0,0 +1,39 @@
+import os
+from pathlib import Path
+import torch
+from datasets.mnist_datamodule import MNISTDataModule
+from models.gan import GAN
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks.progress import TQDMProgressBar
+
+
+def main():
+    root_dir = Path(__file__).parent.parent
+    data_dir = root_dir / "data"
+    num_workers = os.cpu_count() - 2
+    num_epochs = 100
+    callbacks = [TQDMProgressBar(refresh_rate=20)]
+
+    datamodule = MNISTDataModule(
+        data_dir=data_dir,
+        num_workers=num_workers,
+    )
+    model = GAN(
+        *datamodule.dims,
+        lr=0.0002,
+        batch_size=256,
+        b1=0.7,
+        b2=0.999,
+    )
+    trainer = pl.Trainer(
+        accelerator="mps",
+        devices=1 if torch.backends.mps.is_available() else None,
+        max_epochs=num_epochs,
+        callbacks=callbacks,
+    )
+    trainer.fit(model, datamodule=datamodule)
+
+
+if __name__ == "__main__":
+    pl.seed_everything(1234)
+    main()
